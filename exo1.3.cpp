6d139ca155b35bf64fbba574a2beae23f856adae
RemiMarty <marty.remi@lyceejulesfil.fr>
2023-09-22 14:37:38 +0200

    exo1.3

--- exo1.3.cpp
+++ exo1.3.cpp
@@ -5,6 +5,7 @@ int main()
 {
      int age_utilisateur(18);
     cout << "L'age de l'utilisateur est : ";
-    cout << age_utilisateur; cout << " ans." << endl;
+    cout << age_utilisateur;
+    cout << " ans." << endl;
     return 0;
 }
