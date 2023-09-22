55bd3d73c16098a7869ce11250226131b336ccdb
RemiMarty <marty.remi@lyceejulesfil.fr>
2023-09-22 14:57:31 +0200

    exo1.5

--- exo1.5.cpp
+++ exo1.5.cpp
@@ -4,18 +4,13 @@ int main()
 
 {
     double pi(3.14159);
-
     bool lampe(true);
-
     char lettre('A');
     string chaine("C'est une chaine de caracteres");
 
     cout << "Le chiffre pi a comme valeur : " << pi << "." << endl;
-
     cout << "La lampe est : " << lampe << " (0 pour eteinte et 1 pour allumee)." << endl;
-
     cout << "La lettre a afficher est : " << lettre << "." << endl;
-
     cout << "La chaine de caracteres a afficher est : " << chaine << "." << endl;
 
     return 0;
